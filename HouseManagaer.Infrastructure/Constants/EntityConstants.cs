99bc0da7207b79bc374ce6bccd21264c86a167ac
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-11 16:36:51 +0200

    Remove constants for the names of the occupants. The name is placed in one field named FullName and the constants are chabged accrodingly.

--- HouseManagaer.Infrastructure/Constants/EntityConstants.cs
+++ HouseManagaer.Infrastructure/Constants/EntityConstants.cs
@@ -8,7 +8,10 @@
 		public const int HouseOrganizationNameMinLength = 3;
 		public const int HouseOrganizationNameMaxLength = 20;
 
-		public const int AddressMinLength = 10;
+        public const int BoardMemberFullNameMinLength = 10;
+        public const int BoardMemberFullNameMaxLength = 100;
+
+        public const int AddressMinLength = 10;
 		public const int AddressMaxLength = 100;
 
 		public const int TownNameMinLength = 3;
@@ -17,16 +20,10 @@
 		public const int UnitTypeNameMinLength = 3;
 		public const int UnitTypeNameMaxLength = 20;
 
-		public const int FirstNameMinLength = 2;
-		public const int FirstNameMaxLength = 20;
-		
-		public const int MiddleNameMinLength = 2;
-		public const int MiddleNameMaxLength = 30;
-		
-		public const int LastNameMinLength = 2;
-		public const int LastNameMaxLength = 50;
+        public const int OccupantFullNameMinLength = 10;
+        public const int OccupantFullNameMaxLength = 100;
 
-		public const string UnitCommonPartsMinArea = "0";
+        public const string UnitCommonPartsMinArea = "0";
 		public const string UnitCommonPartsMaxArea = "100";
 
 		public const string PhoneNumberRegEx = @"^\+[0-9 ]{8,}$";
