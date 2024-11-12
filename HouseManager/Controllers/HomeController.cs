31b03486304faa5526981616711eeb21e481fc83
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-12 15:19:29 +0200

    Removed temporarily created view and action in the Home Controller

--- HouseManager/Controllers/HomeController.cs
+++ HouseManager/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace HouseManager.Controllers
 			return View();
         }
 
-		public IActionResult LogedIn()
-		{
-			return View();
-		}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
