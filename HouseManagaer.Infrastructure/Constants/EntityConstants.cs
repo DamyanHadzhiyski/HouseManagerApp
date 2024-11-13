fb96a784bcf060f7c923c438030570c851edd693
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-13 11:53:20 +0200

    Added summary for each constant in the EntityConstants

--- HouseManagaer.Infrastructure/Constants/EntityConstants.cs
+++ HouseManagaer.Infrastructure/Constants/EntityConstants.cs
@@ -6,49 +6,49 @@
 	public static class EntityConstants
 	{
 		/// <summary>
-		/// Min and Max length of the description fields
+		/// Min and Max length of a description field
 		/// </summary>
 		public const int DescriptionMinLength = 10;
 		public const int DescriptionMaxLength = 200;
 
         /// <summary>
-        /// Min and Max length of the House Organization name
+        /// Min and Max length of a House Organization name
         /// </summary>
         public const int HouseOrganizationNameMinLength = 3;
 		public const int HouseOrganizationNameMaxLength = 50;
 
         /// <summary>
-        /// Min and Max length of the 
+        /// Min and Max length of a person names
         /// </summary>
         public const int FullNameMinLength = 10;
         public const int FullNameMaxLength = 100;
 
         /// <summary>
-        /// Min and Max length of the
+        /// Min and Max length of an address
         /// </summary>
         public const int AddressMinLength = 10;
 		public const int AddressMaxLength = 100;
 
         /// <summary>
-        /// Min and Max length of the
+        /// Min and Max length of a name of the town
         /// </summary>
         public const int TownNameMinLength = 3;
 		public const int TownNameMaxLength = 20;
 
         /// <summary>
-        /// Min and Max length of the
+        /// Min and Max length of a unit type
         /// </summary>
 		public const int UnitTypeNameMinLength = 3;
 		public const int UnitTypeNameMaxLength = 20;
 
         /// <summary>
-        /// Min and Max length of the
+        /// Min and Max values of a units' common parts
         /// </summary>
         public const string UnitCommonPartsMinArea = "0";
 		public const string UnitCommonPartsMaxArea = "100";
 
         /// <summary>
-        /// Min and Max length of the
+        /// Requested phone number format
         /// </summary>
 		public const string PhoneNumberRegEx = @"^\+[0-9 ]{8,}$";
 	}
