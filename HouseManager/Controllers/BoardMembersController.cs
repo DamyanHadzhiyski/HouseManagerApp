e5b0887e5871065cdd1a401ff6f03683a34493f3
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-14 14:37:14 +0200

    Implemented functionalities for adding and showing all board members

--- HouseManager/Controllers/BoardMembersController.cs
+++ HouseManager/Controllers/BoardMembersController.cs
@@ -1,20 +1,75 @@
-﻿using HouseManager.Core.Models.BoardMember;
+﻿
+using HouseManager.Core.Contracts;
+using HouseManager.Core.Models.BoardMember;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseManager.Controllers
 {
-	public class BoardMembersController : BaseController
+	public class BoardMembersController(
+					IBoardMemberService boardMemberService) : BaseController
 	{
 		#region Show All Board Members
-		public IActionResult All()
+		public async Task<IActionResult> All()
+		{
+			var model = await boardMemberService
+								.GetAllReadonlyAsync()
+								.Select(bm => new BoardMemberModel
+								{
+									Id = bm.Id,
+									Name = bm.Name,
+									Position = bm.Position,
+									StartDate = bm.StartDate,
+									EndDate = bm.EndDate,
+									PhoneNumber = bm.PhoneNumber
+								})
+								.ToListAsync();
+
+			return View(model);
+		}
+		#endregion
+
+		#region Add New Board member
+		[HttpGet]
+		public IActionResult Add()
 		{
 			var model = new BoardMemberModel();
+			
+			return View(model);
+		}
 
+		public async Task<IActionResult> Add(BoardMemberModel model)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			await boardMemberService.AddBoardMemberAsync(model);
+
+			return RedirectToAction(nameof(All));
+		}
+		#endregion
+
+		#region Edit Board Member
+		public async Task<IActionResult> Edit(int id)
+		{
 
 		}
 		#endregion
 
+		#region Private Methods
+		//private List<SelectListItem> GetMemberPositions()
+		//{
+		//	var positions = new List<SelectListItem>();
 
+		//	foreach (var position in Enum.BoardMemberPosition)
+		//	{
+		//		positions.Add(new )
+		//	}
+
+		//}
+		#endregion
 	}
 }
