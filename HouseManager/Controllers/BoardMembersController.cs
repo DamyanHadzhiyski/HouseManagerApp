8e545e89219046a0a5b5b3c7607f629ad7d7ec29
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-14 10:05:10 +0200

    Added Board Members controller

---
+++ HouseManager/Controllers/BoardMembersController.cs
@@ -0,0 +1,20 @@
+﻿using HouseManager.Core.Models.BoardMember;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseManager.Controllers
+{
+	public class BoardMembersController : BaseController
+	{
+		#region Show All Board Members
+		public IActionResult All()
+		{
+			var model = new BoardMemberModel();
+
+
+		}
+		#endregion
+
+
+	}
+}
