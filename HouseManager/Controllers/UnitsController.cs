67453e142fcc1cc3c1c66a223a104a80e1502450
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-18 15:05:36 +0200

    Added using fo Unit models

--- HouseManager/Controllers/UnitsController.cs
+++ HouseManager/Controllers/UnitsController.cs
@@ -1,12 +1,65 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HouseManager.Core.Contracts;
+using HouseManager.Core.Models.Unit;
+using HouseManager.Core.Services;
+using HouseManager.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseManager.Controllers
 {
-	public class UnitsController : BaseController
+    public class UnitsController(
+		HouseManagerDbContext context,
+		IUnitService unitService) : BaseController
 	{
-		public IActionResult Index()
+        #region Show All Units
+        [HttpGet]
+		public async Task<IActionResult> All()
 		{
-			return View();
+			var model = await unitService.GetAllUnitsAsync();
+
+			return View(model);
 		}
+		#endregion
+
+		#region Add New Unit
+		[HttpGet]
+        public async Task<IActionResult> Add()
+		{
+			var model = new UnitModel();
+
+			ViewBag.UnitTypes = await GetUnitTypes();
+
+			return View(model);
+		}
+
+		public async Task<IActionResult> Add(UnitModel model)
+		{
+			if(!ModelState.IsValid)
+			{
+				ViewBag.UnitTypes = await GetUnitTypes();
+
+				//TODO: Add Exception
+
+				return View(model);
+			}
+
+			return RedirectToAction(nameof(All));
+		}
+        #endregion
+
+        #region Private Methods
+        private async Task<List<SelectListItem>> GetUnitTypes()
+		{
+			return await context.UnitTypes
+								.Select(ut => new SelectListItem()
+								{
+									Text = ut.Name,
+									Value = ut.Id.ToString()
+								})
+								.ToListAsync();
+		}
+		#endregion
 	}
 }
