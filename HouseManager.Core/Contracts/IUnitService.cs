ee9aab38e96271a5781855daa22789b87e6eacbf
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-18 15:04:35 +0200

    Implemented Unit interface and service, along with the models used in the top layers of the app.

---
+++ HouseManager.Core/Contracts/IUnitService.cs
@@ -0,0 +1,14 @@
+﻿using HouseManager.Core.Models.Unit;
+using HouseManager.Infrastructure.Data.Models;
+
+namespace HouseManager.Core.Contracts
+{
+    public interface IUnitService
+    {
+        //Task EditUnitAsync(UnitModel model);
+
+        Task<List<UnitModel>> GetAllUnitsAsync();
+
+        Task<Unit?> GetUnitByIdAsync(int id);
+    }
+}
---
+++ HouseManager.Core/Services/UnitService.cs
@@ -0,0 +1,40 @@
+﻿
+using HouseManager.Core.Contracts;
+using HouseManager.Core.Models.Unit;
+using HouseManager.Infrastructure.Data;
+using HouseManager.Infrastructure.Data.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseManager.Core.Services
+{
+    public class UnitService(
+        HouseManagerDbContext context) : IUnitService
+    {
+        //public async Task EditUnitAsync(UnitModel model)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public async Task<List<UnitModel>> GetAllUnitsAsync()
+        {
+            return await context.Units
+                            .Select(u => new UnitModel
+                            {
+                                Id = u.Id,
+                                Number = u.UnitNumber,
+                                Floor = u.Floor,
+                                TypeId = u.UnitTypeId,
+                                TotalArea = u.TotalArea,
+                                CommonParts = u.CommonParts
+                            })
+                            .ToListAsync();
+        }
+
+        public async Task<Unit?> GetUnitByIdAsync(int id)
+        {
+            return await context.Units
+                            .FirstOrDefaultAsync(u => u.Id == id);
+        }
+    }
+}
