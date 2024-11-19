a259672e6163d5d38aa6508ede5751091c4382c0
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-19 11:56:13 +0200

    Added UnitFormModel

---
+++ HouseManager.Core/Models/Unit/UnitFormModel.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using HouseManager.Infrastructure.Data.Models;
+
+using static HouseManager.Infrastructure.Constants.EntityConstants;
+
+namespace HouseManager.Core.Models.Unit
+{
+	public class UnitFormModel
+	{
+		/// <summary>
+		/// Primary identifier of the unit
+		/// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Identifier of the unit type
+        /// </summary>
+        [Required]
+		public int TypeId { get; set; }
+
+		/// <summary>
+		/// Floor on which the unit is located
+		/// </summary>
+		[Required]
+		public int Floor { get; set; }
+
+		/// <summary>
+		/// Unique number of the unit
+		/// </summary>
+		[Required]
+		public string Number { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Total area of the unit in m2
+		/// </summary>
+		[Required]
+		public decimal TotalArea { get; set; }
+
+		/// <summary>
+		/// Common parts in percentage that are adjacent ot the unit
+		/// </summary>
+		[Required]
+		[Range(typeof(decimal), UnitCommonPartsMinArea, UnitCommonPartsMaxArea)]
+		public decimal CommonParts { get; set; }
+
+		/// <summary>
+		/// Number of pats that are taken out in the common areas
+		/// </summary>
+		public int PetsCount { get; set; }
+
+		/// <summary>
+		/// Occupants that live in the unit
+		/// </summary>
+		public ICollection<Occupant> Occupants { get; set; } = [];
+	}
+}
