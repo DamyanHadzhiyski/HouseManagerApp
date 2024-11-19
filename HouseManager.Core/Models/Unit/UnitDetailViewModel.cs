ec940989a67c9d949b3654916f89d05d64b64149
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-19 11:56:00 +0200

    Renamed UnitModel and UnitDetailModel models to UnitViewModel and UnitDetailViewModel.

---
+++ HouseManager.Core/Models/Unit/UnitDetailViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace HouseManager.Core.Models.Unit
+{
+    /// <summary>
+    /// Model that extends the UnitModel in order to 
+    /// show additional details for the unit
+    /// </summary>
+    public class UnitDetailViewModel : UnitViewModel
+	{
+        /// <summary>
+        /// Total area of the unit in m2
+        /// </summary>
+        public string TotalArea { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Common parts in percentage that are adjacent ot the unit
+        /// </summary>
+        public string CommonParts { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Number of pats that are going out in the common areas
+        /// </summary>
+        public string PetsCount { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The credit/debit of the unit
+        /// </summary>
+        public string Balance { get; set; } = string.Empty;
+
+		//TODO: Add details for the occupants
+	}
+}
---
+++ HouseManager.Core/Models/Unit/UnitViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace HouseManager.Core.Models.Unit
+{
+    /// <summary>
+    /// Model of a house organization used in the top level layers of the app
+    /// </summary>
+    public class UnitViewModel
+	{/// <summary>
+	 /// Primary identifier of the unit
+	 /// </summary>
+
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Type of the unit
+		/// </summary>
+		public string Type { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Floor on which the unit is located
+        /// </summary>
+		public string Floor { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Unique number of the unit
+        /// </summary>
+        public string Number { get; set; } = string.Empty;
+	}
+}
