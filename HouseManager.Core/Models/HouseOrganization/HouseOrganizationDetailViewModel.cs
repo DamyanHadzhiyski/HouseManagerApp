efb21a17319500653a1df892fd5e4281c27a340a
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-20 20:34:38 +0200

    Added View in the names of house organization models

---
+++ HouseManager.Core/Models/HouseOrganization/HouseOrganizationDetailViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseManager.Core.Models.HouseOrganization
+{
+	/// <summary>
+	/// Model that extends the HouseOrganizationModel in order to 
+	/// show additional details for the House Organization
+	/// </summary>
+	public class HouseOrganizationDetailViewModel : HouseOrganizationViewModel
+    {
+		/// <summary>
+		/// Name of the president of the House Organization
+		/// </summary>
+		[Display(Name = "President")]
+        public string PresidentName { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Name of the cashier of the House Organization
+		/// </summary>
+		[Display(Name = "Cashier")]
+		public string CashierName { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Number of units in the House Organization
+		/// </summary>
+		public string UnitsCount { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Number of occupants of the House Organization
+		/// </summary>
+		public string OccupantsCount { get; set; } = string.Empty;
+	}
+}
---
+++ HouseManager.Core/Models/HouseOrganization/HouseOrganizationViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseManager.Core.Models.HouseOrganization
+{
+	/// <summary>
+	/// Model of a house organization used in the top level layers of the app
+	/// </summary>
+	public class HouseOrganizationViewModel
+    {
+        /// <summary>
+        /// Primary identifier of the House Organization
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the House Organization
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Address of the House Organization
+        /// </summary>
+        public string Address { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Name of the town in which the House Organization is located
+        /// </summary>
+        [Display(Name = "Town")]
+        public string TownName { get; set; } = string.Empty;
+    }
+}
