2fe4485d72cd931fda52a96d419633a7af64f3fb
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-24 14:53:28 +0200

    Added message constant, if set value is outside of the required range

--- HouseManager.Core/Constants/ErrorMessages.cs
+++ HouseManager.Core/Constants/ErrorMessages.cs
@@ -2,7 +2,8 @@
 {
     public static class ErrorMessages
     {
-        public const string FieldRequiredErrorMessage = "Field {0} is required";
-        public const string FieldLengthErrorMessage = "Field {0} must be between {2} and {1} characters long!";
+        public const string FieldRequiredErrorMessage = "Field {0} value is required";
+        public const string FieldLengthErrorMessage = "Field {0} value must be between {2} and {1} characters long!";
+        public const string FieldInRangeErrorMessage = "Field {0} value must be between {2} and {1}!";
     }
 }
