23931146f99bad0c61a8fb1951153e09f167dcb1
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-28 10:39:11 +0200

    Added Management controller to visualise president and cashier info

---
+++ HouseManager/Controllers/ManagementController.cs
@@ -0,0 +1,29 @@
+﻿using HouseManager.Core.Contracts;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseManager.Controllers
+{
+	public class ManagementController(
+		IPresidentService presidentService) : BaseController
+	{
+		#region Show All Board Members
+		public async Task<IActionResult> All(int houseOrgId)
+		{
+			ViewBag.ActivePresident = await presidentService
+								.GetActiveReadOnlyAsync(houseOrgId);
+
+			ViewBag.InactivePresidents = await presidentService
+								.GetAllInactiveReadOnlyAsync(houseOrgId);
+
+			ViewBag.ActiveCashier = await presidentService
+								.GetActiveReadOnlyAsync(houseOrgId);
+
+			ViewBag.InactiveCashiers = await presidentService
+								.GetAllInactiveReadOnlyAsync(houseOrgId);
+
+			return View();
+		}
+		#endregion
+	}
+}
