7b68c7abcc690c2b2c692c7eeb8be185ac388d3d
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-28 09:40:54 +0200

    Added custom date picker

--- HouseManager/wwwroot/js/custom/main.js
+++ HouseManager/wwwroot/js/custom/main.js
@@ -1,15 +1,5 @@
-(function ($) {
-    "use strict";
-
-    // Sidebar Toggler
-    $('.sidebar-toggler').click(function () {
-        $('.sidebar, .content').toggleClass("open");
-        return false;
-    });
-})(jQuery);
-
 function getHouseOrg() {
     var e = document.getElementById("houseOrgSelect");
     var id = e.value;
     location.href = "/HouseOrganizations/Manage/" + id;
-}
+};
