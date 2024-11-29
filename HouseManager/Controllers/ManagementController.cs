1c1c49d57e5ef4cdfc59e72aaeef16cde416e9f1
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-29 09:58:00 +0200

    Added logic to retrieve cashiers info from the Management all action

--- HouseManager/Controllers/ManagementController.cs
+++ HouseManager/Controllers/ManagementController.cs
@@ -1,26 +1,66 @@
 ﻿using HouseManager.Core.Contracts;
+using HouseManager.Core.Models.Manager;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseManager.Controllers
 {
 	public class ManagementController(
-		IPresidentService presidentService) : BaseController
+		IPresidentService presidentService,
+		ICashierService cashierService) : BaseController
 	{
 		#region Show All Board Members
+		[HttpGet]
 		public async Task<IActionResult> All(int houseOrgId)
 		{
 			ViewBag.ActivePresident = await presidentService
-								.GetActiveReadOnlyAsync(houseOrgId);
+									.GetActiveReadOnlyAsync(houseOrgId)
+									.Select(p => new Class1
+									{
+										Id = p.Id,
+										Name = p.Name,
+										PhoneNumber = p.PhoneNumber,
+										StartDate = p.StartDate,
+										EndDate = p.EndDate,
+										Progress = p.Progress
+									})
+									.FirstOrDefaultAsync();
 
 			ViewBag.InactivePresidents = await presidentService
-								.GetAllInactiveReadOnlyAsync(houseOrgId);
+												.GetAllInactiveReadOnlyAsync(houseOrgId)
+												.Select(p => new InactiveManagementViewModel
+												{
+													Name = p.Name,
+													StartDate = p.StartDate,
+													EndDate = p.EndDate,
+													TerminationDate = p.TerminationDate
+												})
+												.ToListAsync();
 
-			ViewBag.ActiveCashier = await presidentService
-								.GetActiveReadOnlyAsync(houseOrgId);
+			ViewBag.ActiveCashier = await cashierService
+									.GetActiveReadOnlyAsync(houseOrgId)
+									.Select(p => new Class2
+									{
+										Id = p.Id,
+										Name = p.Name,
+										PhoneNumber = p.PhoneNumber,
+										StartDate = p.StartDate,
+										EndDate = p.EndDate,
+										Progress = p.Progress
+									})
+									.FirstOrDefaultAsync();
 
-			ViewBag.InactiveCashiers = await presidentService
-								.GetAllInactiveReadOnlyAsync(houseOrgId);
+			ViewBag.InactiveCashiers = await cashierService
+												.GetAllInactiveReadOnlyAsync(houseOrgId)
+												.Select(p => new InactiveManagementViewModel
+												{
+													Name = p.Name,
+													StartDate = p.StartDate,
+													EndDate = p.EndDate,
+													TerminationDate = p.TerminationDate
+												})
+												.ToListAsync();
 
 			return View();
 		}
