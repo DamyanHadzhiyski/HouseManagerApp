e32729193af8126c8a6f1b8a28948d1da6d2965e
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-29 09:53:47 +0200

    Implemented views for visualising management information

---
+++ HouseManager.Core/Models/Manager/ActiveManagementFormModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static HouseManager.Core.Constants.ErrorMessages;
+using static HouseManager.Infrastructure.Constants.EntityConstants;
+
+namespace HouseManager.Core.Models.Manager
+{
+	/// <summary>
+	/// Model of a board member used in the top level layers of the app
+	/// </summary>
+	public class ActiveManagementFormModel : IValidatableObject
+    {
+        /// <summary>
+        /// Primary identifier of the board member
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Manager name
+        /// </summary>
+        [Required(ErrorMessage = FieldRequiredErrorMessage)]
+        [StringLength(FullNameMaxLength,
+            MinimumLength = FullNameMinLength,
+            ErrorMessage = FieldLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        public string ManagerPosition { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Start date of assignment to the management position
+        /// </summary>
+        [Required(ErrorMessage = FieldRequiredErrorMessage)]
+        public DateTime StartDate { get; set; }
+
+		/// <summary>
+		/// Due date of assignment to the management position
+		/// </summary>
+		[Required(ErrorMessage = FieldRequiredErrorMessage)]
+		public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Phone number of the manager
+        /// </summary>
+        [Required(ErrorMessage = FieldRequiredErrorMessage)]
+        [RegularExpression(PhoneNumberRegEx)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+		/// <summary>
+		/// House organization managed by the manager
+		/// </summary>
+		[Required(ErrorMessage = FieldRequiredErrorMessage)]
+		public int HouseOrganizationId { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			
+			if(StartDate > EndDate)
+			{
+				yield return new ValidationResult("End Date cannot be before Start Date");
+			}
+		}
+	}
+}
---
+++ HouseManager.Core/Models/Manager/InactiveManagementViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseManager.Core.Models.Manager
+{
+	public class InactiveManagementViewModel
+	{
+		/// <summary>
+		/// President name
+		/// </summary>
+		public string Name { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Start date of assignment to the president position
+		/// </summary>
+		[Display(Name = "Start Date")]
+		public string StartDate { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Due date of assignment to the president position
+		/// </summary>
+		[Display(Name = "End Date")]
+		public string EndDate { get; set; } = string.Empty;
+
+		/// <summary>
+		/// Termination date of the president
+		/// </summary>
+		[Display(Name = "Termination Date")]
+		public string TerminationDate { get; set; } = string.Empty;
+	}
+}
