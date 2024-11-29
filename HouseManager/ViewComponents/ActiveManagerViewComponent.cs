bee461254d8ab984c9aaa1815c4613e4eab5ed71
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-11-29 09:57:03 +0200

    Created ViewCompnent for Active management visualisation

---
+++ HouseManager/ViewComponents/ActiveManagerViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using HouseManager.Core.Models.Manager;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseManager.ViewComponents
+{
+	public class ActiveManagerViewComponent() : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync(ActiveManagementViewModel model, string position)
+		{
+			if (model == null)
+			{
+				var emptyModel = new ActiveManagementFormModel();
+
+				emptyModel.ManagerPosition = position;
+
+				return View("_ActiveManagerFormPartial", emptyModel);
+			}
+			else
+			{
+				return View("_ActiveManagerViewPartial",model);
+			}
+
+		}
+	}
+}
