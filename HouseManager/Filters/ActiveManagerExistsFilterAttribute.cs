cd7bcc19eef542c8a1df735c1aa97e71d889d7d4
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-12-16 13:01:55 +0200

    Added custom filter attributes for existance check of the manager or occupant

---
+++ HouseManager/Filters/ActiveManagerExistsFilterAttribute.cs
@@ -0,0 +1,37 @@
+﻿
+using HouseManager.Core.Contracts;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HouseManager.Filters
+{
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	public class ActiveManagerExistsFilterAttribute : Attribute, IAsyncActionFilter
+	{
+		private readonly IManagementService managementService;
+		private int managerId;
+
+        public ActiveManagerExistsFilterAttribute(IManagementService _managementService)
+        {
+			managementService = _managementService;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			if (int.TryParse(context.RouteData.Values["id"]?.ToString(), out managerId) == false)
+			{
+				context.Result = new BadRequestResult();
+				return;
+			}
+
+			if (!await managementService.IsActiveAsync(managerId))
+			{
+				context.Result = new NotFoundResult();
+				return;
+			}
+
+			await next();
+		}
+	}
+}
---
+++ HouseManager/Filters/ActiveOccupantExistsFilterAttribute.cs
@@ -0,0 +1,37 @@
+﻿
+using HouseManager.Core.Contracts;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HouseManager.Filters
+{
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	public class ActiveOccupantExistsFilterAttribute : Attribute, IAsyncActionFilter
+	{
+		private readonly IOccupantService occupantService;
+		private int occupantId;
+
+        public ActiveOccupantExistsFilterAttribute(IOccupantService _occupantService)
+        {
+            occupantService = _occupantService;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			if (int.TryParse(context.RouteData.Values["id"]?.ToString(), out occupantId) == false)
+			{
+				context.Result = new BadRequestResult();
+				return;
+			}
+
+			if (!await occupantService.IsActiveAsync(occupantId))
+			{
+				context.Result = new NotFoundResult();
+				return;
+			}
+
+			await next();
+		}
+	}
+}
