57d32da92ed5424e4ed57ee11998a2653c8f1400
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2024-12-16 13:00:21 +0200

    Renamed UsersService and interface to UserService

---
+++ HouseManager.Core/Contracts/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HouseManager.Core.Contracts
+{
+	public interface IUserService
+	{
+		Task AddToRoleAsync(string userId, string roleName);
+
+		Task<IdentityUser> GetByIdAsync(string id);
+
+		Task<ICollection<string>> GetRolesByIdAsync(string userId);
+
+		Task<string> GetCurrentRoleAsync(string userId);
+
+		Task SetCurrentRoleAsync(string userId, string role);
+	}
+}
---
+++ HouseManager.Core/Services/UserService.cs
@@ -0,0 +1,64 @@
+﻿using HouseManager.Core.Contracts;
+using HouseManager.Infrastructure.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using static HouseManager.Infrastructure.Constants.UserClaims;
+
+namespace HouseManager.Core.Services
+{
+	public class UserService(
+		HouseManagerDbContext context,
+		UserManager<IdentityUser> userManager) : IUserService
+	{
+		public async Task AddToRoleAsync(string userId, string roleName)
+		{
+			var user = await GetByIdAsync(userId);
+
+			var assignedRoles = await GetRolesByIdAsync(userId);
+
+			if (!assignedRoles.Contains(roleName))
+			{
+				await userManager.AddToRoleAsync(user, roleName);
+			}
+			else
+			{
+				return;
+			}
+		}
+
+		public async Task<IdentityUser> GetByIdAsync(string id)
+		{
+			return await context.Users.FindAsync(id);
+		}
+
+		public async Task<string> GetCurrentRoleAsync(string userId)
+		{
+			return await context.UserClaims
+							.Where(uc => uc.UserId == userId
+											&& uc.ClaimType == CurrentRole)
+							.Select(uc => uc.ClaimValue)
+							.FirstOrDefaultAsync();
+		}
+
+		public async Task<ICollection<string>> GetRolesByIdAsync(string userId)
+		{
+			var user = await GetByIdAsync(userId);
+
+			return await userManager.GetRolesAsync(user);
+		}
+
+		public async Task SetCurrentRoleAsync(string userId, string role)
+		{
+			var userClaim = await context.UserClaims
+							.Where(uc => uc.UserId == userId
+											&& uc.ClaimType == CurrentRole)
+							.FirstOrDefaultAsync();
+
+			userClaim.ClaimValue = role;
+
+			await context.SaveChangesAsync();
+		}
+	}
+}
